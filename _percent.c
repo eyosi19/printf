f2b284997abc4eb6dd02f100bd54ed8124545def
Eyosiyas <xoyosi19@gmail.com>
2023-12-05 14:30:10 -0500

    TEAMWORK

---
+++ _percent.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _percent - return percent
+ *
+ * Return: 1
+ */
+
+int _percent(void)
+{
+	_putchar('%');
+	return (1);
+}
--- _string.c
+++ _string.c
@@ -10,5 +10,32 @@
 
 int _string(va_list list)
 {
+	char *str;
+	char i;
+	int len;
 
+	str = va_arg(list, char *);
+
+	if (str == NULL)
+	{
+		str == "(null)";
+		len = _strlen(str);
+
+		for (i = 0; i < len; i++)
+		{
+			_putchar(str[i]);
+		}
+		return (len);
+	}
+	else
+	{
+		len = _strlen(str);
+
+		for (i = 0; i < len; i++)
+		{
+			_putchar(str[i]);
+		}
+
+		return (len);
+	}
 }
