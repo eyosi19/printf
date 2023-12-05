cc3ccfc3b20456cb72278e76beb7e9ef62a6132b
Eyosiyas <xoyosi19@gmail.com>
2023-12-05 18:44:55 -0500

    TEAMWORK

--- _hexadecimal.c
+++ _hexadecimal.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _octal - converts unsigned int to octal
+ * _hexadecimal - converts unsigned int to octal
  *
  * @list: the argument
  *
@@ -15,9 +15,9 @@ int _hexadecimal(va_list list)
 	unsigned int n = va_arg(list, unsigned int);
 	unsigned int t = n;
 
-	while (n / 8 != 0)
+	while (n / 16 != 0)
 	{
-		n /= 8;
+		n /= 16;
 		count++;
 	}
 	count++;
@@ -26,8 +26,8 @@ int _hexadecimal(va_list list)
 
 	for (i = 0; i < count; i++)
 	{
-		arr[i] = t % 8;
-		t /= 8;
+		arr[i] = t % 16;
+		t /= 16;
 	}
 
 	for (i = count - 1; i >= 0; i--)
